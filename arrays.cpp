46276b091387d7840542acc1694a125865e6ef19
Regan <52940978+reganjs@users.noreply.github.com>
2021-03-11 21:00:08 -0600

    Update and rename TMA1Question4.cpp to arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,6 +1,4 @@
 //Regan Setter
-//Student ID 3505894
-//Computer Science 206
 //January 17th, 2021
 
 //Write a program that creates an array of 100 string objects. Fill the array by having your program open a (text) file and read one line of the file into each string until you have filled the array. Display the array using the format “line #: <string>,” where # is the actual line number (you can use the array counter for this value) and <string> is the stored string.
